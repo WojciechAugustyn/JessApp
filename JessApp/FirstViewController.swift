7085ce97118183bd7e76220113cbb02f8554bf36
Wojtek <wojciech.augustyn85@gmail.com>
2018-11-07 23:19:10 +0000

    Initial Commit

---
+++ JessApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  JessApp
+//
+//  Created by Wojciech Augustyn on 07.11.2018.
+//  Copyright © 2018 Wojciech Augustyn. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
